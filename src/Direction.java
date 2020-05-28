925e7b300868acf26d3e15d21ce19c201758311b
kashyapnathan <43067072+kashyapnathan@users.noreply.github.com>
2020-05-27 22:35:04 -0400

    Add files via upload

--- src/Direction.java
+++ src/Direction.java
@@ -1,3 +1,4 @@
+package pacman;
 
 public enum Direction {
 
--- src/Tile.java
+++ src/Tile.java
@@ -1,3 +1,4 @@
+package pacman;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -15,9 +16,18 @@ public class Tile extends JComponent {
 	public void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
 		g2.setColor(Color.BLUE);
-		g2.fill(new Rectangle2D.Double(0, 0, 15, 15));
+		g2.fill(new Rectangle2D.Double(getX(), getY(), 31, 31));
 		g2.setColor(Color.WHITE);
-		g2.draw(new Rectangle2D.Double(0, 0, 15, 15));
+		g2.draw(new Rectangle2D.Double(getX(), getY(), 31, 31));
+	}
+	
+	public boolean checkCollision(JComponent other) {
+		return ((other.getX() < getX() + getWidth()) && (other.getY() < getY() + getHeight()) && (other.getY() + other.getHeight() > getY()) && (other.getX() + other.getWidth() > getX()));
+	}
+	
+	public boolean checkCollision(int x, int y, int changex, int changey) {
+		Tile tile = new Tile(x + changex, y + changey);
+		return (checkCollision(tile));
 	}
 	
 }
